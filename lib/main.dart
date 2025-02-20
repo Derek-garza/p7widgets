396411496a4519bf31d37103d4aeae984197b4f1
DEREK_SANTIAGO_GARZA__ <a.22308051281196@cbtis128.edu.mx>
2025-02-20 19:34:27 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiWidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiWidgets extends StatelessWidget {
+  const MiWidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Garza ",
+            style: TextStyle(color: Color(0xff9d742a)),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        body: const Column(
+          children: <Widget>[
+            Text(
+              'Derek Garza Martinez',
+              style: TextStyle(color: Color(0xff9d742a), fontSize: 30),
+            ),
+            Text(
+              'Matricula:22308051281196 gpo 6-J',
+              style: TextStyle(color: Color(0xff000000), fontSize: 20),
+            ),
+            Expanded(
+              child: FittedBox(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
         ),
       ),
-    );
+    ); // fin de material
   }
 }
